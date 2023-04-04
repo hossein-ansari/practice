a6e25e3ac5677085548942eae8ea96be0db7ed20
hossein <hossein838485@gmail.com>
2023-04-04 17:27:04 +0430

    add change color

--- src/App.js
+++ src/App.js
@@ -5,26 +5,39 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0 
+      count: 0,
+      color: "blue",
     };
   }
 
   upHandler() {
+    if (this.state.count > 5) {
+      return false;
+    }
     this.setState((prevstate) => {
-      return {count : prevstate.count + 1}
+      return { count: prevstate.count + 1 };
     });
+    if (this.state.count >= 3) {
+      this.setState({ color: "red" });
+    }
   }
   downHandler() {
+    if (this.state.count <= 0) {
+      return false;
+    }
     this.setState((prevstate) => {
-  
-      return {count : prevstate.count - 1}
+      return { count: prevstate.count - 1 };
     });
+    if (this.state.count <= 3) {
+      this.setState({ color: "blue" });
+    }
   }
-
   render() {
     return (
-      <div>
-        <button onClick={this.upHandler.bind(this)}>up</button>
+      <div id={this.state.color}>
+        <button id="upBtn" onClick={this.upHandler.bind(this)}>
+          up
+        </button>
         <h1>{this.state.count}</h1>
         <button onClick={this.downHandler.bind(this)}>down</button>
       </div>
