b1d70dfef47329904cef85dfa25d7ad53a4e6869
hossein <hossein838485@gmail.com>
2023-04-09 12:13:34 +0430

    add inputs

--- src/App.js
+++ src/App.js
@@ -6,18 +6,41 @@ class App extends Component {
     super();
     this.state = {
       countriesData: [
-        {iran:['tabriz','fars','esfahan','mashhad']},
-        {unitedStates:['chicago','Los Angeles','san diego']},
-        {canada:['Toronto','Vancouver','Montreal',]}
+        { iran: ["tabriz", "fars", "esfahan", "mashhad"] },
+        { unitedStates: ["chicago", "Los Angeles", "san diego"] },
+        { canada: ["Toronto", "Vancouver", "Montreal"] },
       ],
+      countrySelected: "",
+      countriesData : ["Toronto", "Vancouver", "Montreal"]
     };
   }
   render() {
-    return <div>
+    return (
       <div>
-        <form action=""><input type="chekbox" />select your country</form>
+        <div>
+          <form action="">
+            {/* /* country select */}
+            <select name="" id="">
+              <option value="iran">select</option>
+              <option value="iran">iran</option>
+              <option value="unitedStates">unitedStates</option>
+              <option value="canada">canada</option>
+            </select>
+            {/* /* city select */}
+
+            <select name="" id="">
+              
+              {this.state.countrySelected == false ? (<option value=''>select</option>)
+               :this.state.countriesData.map((city)=>{
+                  return <option value={city}>{city}</option>
+                })
+              }
+            </select>
+            <input type="submit" />
+          </form>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
 
