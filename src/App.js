e66189f5e87a1fc24b55853cc54420cf6a9ffbec
hossein <hossein838485@gmail.com>
2023-04-12 12:46:58 +0430

    change project

--- src/App.js
+++ src/App.js
@@ -1,70 +1,15 @@
-import "./App.css";
-import React, { Component } from "react";
-import Carts from "./Carts";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      countriesData: {
-        iran: ["tabriz", "fars", "esfahan", "mashhad"],
-        unitedStates: ["chicago", "Los Angeles", "san diego"],
-        canada: ["Toronto", "Vancouver", "Montreal"],
-      },
-      users: [
-        { id: 0, name: "ali" },
-        { id: 1, name: "hossein" },
-        { id: 2, name: "mmd" },
-      ],
-      countrySelected: "",
-      citiesData: [],
-    };
-  }
-  selectCountryHandler(e) {
-    let targetCountry = e.target.value;
+import React, { Component } from 'react'
+import Header  from './components/Header/Header'
+import AddForm from './components/BookList/AddForm'
 
-    this.setState({ countrySelected: `${targetCountry}` });
-    this.setState({ citiesData: this.state.countriesData[targetCountry] });
-    console.log(this.state.countriesData[targetCountry]);
-  }
-  removeHandler(id) {
-    console.log(id);
-  }
-  render() {
-    return (
-      <div>
-        <div>
-          <form action="">
-            {/* /* country select */}
-            <select
-              onChange={this.selectCountryHandler.bind(this)}
-              name=""
-              id=""
-            >
-              <option value="iran">select</option>
-              <option value="iran">iran</option>
-              <option value="unitedStates">unitedStates</option>
-              <option value="canada">canada</option>
-            </select>
-            {/* /* city select */}
+export default class App extends Component {
 
-            <select name="" id="">
-              {this.state.countrySelected == false ? (
-                <option value="">select</option>
-              ) : (
-                this.state.citiesData.map((city) => {
-                  return <option value={city}>{city}</option>;
-                })
-              )}
-            </select>
-            <input type="submit" />
-          </form>
-        </div>
-        {this.state.users.map((user) => (
-          <Carts key={user.id}{...user} onRemove={this.removeHandler} />
-        ))}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='container mt-4'>
+                <Header></Header>
+                <AddForm></AddForm>
+            </div>
+        )
+    }
 }
-
-export default App;
