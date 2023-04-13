060736416b2720ff033b968d84634cc6e24a5ecd
hossein <hossein838485@gmail.com>
2023-04-13 22:45:21 +0430

    change project

--- src/components/BookList/AddForm.js
+++ src/components/BookList/AddForm.js
@@ -10,64 +10,7 @@ export default class AddForm extends Component {
 
   render() {
     return (
-      <>
-        <form
-          onSubmit={this.addBookHandler.bind(this)}
-          id="book-form"
-          autocomplete="off"
-        >
-          <div>
-            <label for="title">Title</label>
-            <input
-              value={this.state.title}
-              onChange={(e) => {
-                this.titleInput(e);
-              }}
-              type="text"
-              id="title"
-            />
-          </div>
-
-          <div>
-            <label for="author">Author</label>
-            <input
-              value={this.state.author}
-              onChange={(e) => {
-                this.authorInput(e);
-              }}
-              type="text"
-              id="author"
-            />
-          </div>
-
-          <div>
-            <label for="year">Year</label>
-            <input
-              value={this.state.year}
-              onChange={(e) => {
-                this.yearInput(e);
-              }}
-              type="text"
-              id="year"
-            />
-          </div>
-          <input type="submit" value="Add Book" />
-        </form>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody id="book-list">
-            {this.state.books.map((book) => (
-              <Book {...book} key={book.id} />
-            ))}
-          </tbody>
-        </table>
-      </>
-    );
+     
+    
   }
 }
