8567844a9498068c8479a5d131548cd9a1198c7d
hossein <hossein838485@gmail.com>
2023-04-14 19:22:48 +0430

    add files of todo list

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ export default class App extends Component {
     render() {
         return (
             <div className='container mt-4'>
-                <Header></Header>
-                <AddForm></AddForm>
+                
             </div>
         )
     }
