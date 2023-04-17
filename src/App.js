89f449d8dad02c7aa6c6fcbc654b03ed2f27ede8
hossein <hossein838485@gmail.com>
2023-04-17 17:58:47 +0430

    change app file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
-import TodoList from './components/TodoList/TodoList'
 import './App.css'
 
 export default class App extends Component {
     constructor(props){
         super(props)
         this.state={
-            
         }
     }
     render() {
         return (
-            <div>
-                <TodoList {...this.state} ></TodoList>
-            </div>
+            <div></div>
         )
     }
 }
