51d0367d48c99d0cd8fd12ace4b96fa3e84e8586
hossein <hossein838485@gmail.com>
2023-04-19 20:27:16 +0430

    change project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Alert, Button,Modal } from "react-bootstrap";
+import { Alert, Button,Modal,Row,Col,Container } from "react-bootstrap";
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -10,28 +10,7 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
-        <Alert variant="info">
-          Test MSG <Alert.Link href="#">Home</Alert.Link>
-        </Alert>
-        <Button variant="info" size="">
-          click
-        </Button>
-        <Modal show={this.state.showModal} onHide={()=> this.setState({showModal: false})}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            <p>Modal body text goes here.</p>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="secondary">Close</Button>
-            <Button variant="primary">Save changes</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+      <div></div>
     );
   }
 }
