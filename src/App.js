f8001f7f0e9c5f63a78d7bce336fbf00e227aee6
hossein <hossein838485@gmail.com>
2023-04-19 20:28:19 +0430

    change project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Alert, Button,Modal,Row,Col,Container } from "react-bootstrap";
 export default class App extends Component {
   constructor(props) {
     super(props);
