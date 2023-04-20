e50d2af89359e59a9c0afa92e412d37d909eb4f2
hossein <hossein838485@gmail.com>
2023-04-20 19:16:53 +0430

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default class App extends Component {
         showModal:true
     };
   }
+  componentDidMount(){
+    console.log('mounted');
+  }
   render() {
     return (
       <div></div>
