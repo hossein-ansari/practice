c6ac2e47e74e5f5178dd1d71be3e39728c9618ea
hossein <hossein838485@gmail.com>
2023-04-26 19:16:20 +0430

    try fetch api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, { Component ,PureComponent } from "react";
-import "./App.css";
-export default class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-        showModal:true
-    };
-  }
-  static getDerivedStateFromProps(){
-    console.log('getDerivedStateFromProps');
-  }
-  componentDidMount(){
-    console.log('mounted');
-  }
-  shouldComponentUpdate(props,state){
-    console.log('update');
-
-  }
-  render() {
-    return (
-      <div>
-        <button><input type="text" /></button>
-      </div>
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(
+    (response) => response.json()
+    )
+  .then(
+    (json) => console.log(json)
     );
-  }
-}
