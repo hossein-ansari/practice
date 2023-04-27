3ebe7a1023122f21df9c343de7d3516e16f36413
hossein <hossein838485@gmail.com>
2023-04-27 19:04:48 +0430

    end practice of fetch(POST,DELETE,GET)

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ export default class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
       body: this.state.formData,
-    })
+    }).then(Response=>Response.json())
+    .then(data=>console.log(data))
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "DELETE",
       
     }).then(Response=>Response.json())
       .then(data=>console.log(data))
-
   }
   render() {
 
