e1dfe5b5f5931d6e25f29be7328689a0c7152872
hossein <hossein838485@gmail.com>
2023-04-27 18:46:42 +0430

    practice on fake API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      data:null
-    }
-  }    
-  componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/posts',{
-        method : 'GET'
-      })
-        .then(Response=>{Response.json()})
-        .then(data =>{this.setState({data:data})})
-    }
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "GET",
+    })
+      .then((Response) => Response.json())
+      .then((data) => this.setState({ data: data }));
+  }
   render() {
     console.log(this.state.data);
 
     return (
-      <div>App</div>
-    )
+      <div>
+        {this.state.data.map((posts) => (
+          <h1>{posts.title}</h1>
+        ))}
+      </div>
+    );
   }
 }
-
