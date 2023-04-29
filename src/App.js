1ff534b40aa8bfd067895b7cee289516a7e15d57
hossein <hossein838485@gmail.com>
2023-04-29 19:15:01 +0430

    change all project to react functional component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React, { Component } from "react";
+import React from 'react'
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],    
-      formData:{
-      hossein:'name'
-    }
-    };
-  }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: this.state.formData,
-    }).then(Response=>Response.json())
-    .then(data=>console.log(data))
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "DELETE",
-      
-    }).then(Response=>Response.json())
-      .then(data=>console.log(data))
-  }
-  render() {
-    return (
-      <div>
-        <h2></h2>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>App</div>
+  )
 }
