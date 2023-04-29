19c860cb089239ba5993ed0afc6d676cac4c0e57
hossein <hossein838485@gmail.com>
2023-04-29 19:48:53 +0430

    add filter method

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import React, { useState } from "react";
 export default function App() {
   const [count, setCount] = useState(12);
   const [users, setUsers] = useState([
-    {name :'ali' , last:'ansari' , age:13},
-    {name :'hossein' , last:'ansari' , age:23},
-    {name :'amir' , last:'ansari' , age:58},
-
+    { name: "ali", last: "ansari", age: 13 },
+    { name: "hossein", last: "ansari", age: 23 },
+    { name: "amir", last: "ansari", age: 58 },
   ]);
 
   const up = () => {
     setCount((prevState) => {
       return prevState + 1;
     });
-
   };
   const down = () => {
     setCount((prevState) => {
       return prevState - 1;
+    });
+  };
+  const remove = (name) => {
+      setUsers((prevUser) => {
+      let newUsers=prevUser.filter((user) => {
+        return user.name !== name;
+      });
+      return newUsers
     });
   };
   return (
@@ -25,8 +31,11 @@ export default function App() {
       <button onClick={up}>+</button>
       <h1>{count}</h1>
       <button onClick={down}>-</button>
-      {users.map(user=>(
-        <li>{user.name} {user.last} <button onClick={}></button></li>
+      {users.map((user) => (
+        <li>
+          {user.name} {user.last}{" "}
+          <button onClick={() => remove(user.name)}>-</button>
+        </li>
       ))}
     </div>
   );
