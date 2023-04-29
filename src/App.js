9f3b0bcb312db4664107adf34fe7f31fe7c6f8ad
hossein <hossein838485@gmail.com>
2023-04-29 19:42:20 +0430

    add map method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const [count, setCount]   = useState(12);
+  const [count, setCount] = useState(12);
+  const [users, setUsers] = useState([
+    {name :'ali' , last:'ansari' , age:13},
+    {name :'hossein' , last:'ansari' , age:23},
+    {name :'amir' , last:'ansari' , age:58},
+
+  ]);
+
   const up = () => {
-    setCount(count + 1)
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+
   };
   const down = () => {
-    setCount( count - 1)
+    setCount((prevState) => {
+      return prevState - 1;
+    });
   };
   return (
     <div>
       <button onClick={up}>+</button>
       <h1>{count}</h1>
       <button onClick={down}>-</button>
+      {users.map(user=>(
+        <li>{user.name} {user.last} <button onClick={}></button></li>
+      ))}
     </div>
   );
 }
