297925885c4ee58b0eace2dce2a5607b150019fc
hossein <hossein838485@gmail.com>
2023-04-29 19:19:16 +0430

    new simple project with rfc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function App() {
+  const { count, setCount } = useState(0);
+  const up = () => {
+    setCount( count + 1)
+  };
+  const down = () => {
+    setCount( count - 1)
+  };
   return (
-    <div>App</div>
-  )
+    <div>
+      <button onClick={up}>+</button>
+      <h1>{count}</h1>
+      <button onClick={down}>+</button>
+    </div>
+  );
 }
