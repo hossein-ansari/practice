158cfaf93f282eadb79881606d0b9e728edf03bd
hossein <hossein838485@gmail.com>
2023-05-08 17:29:05 +0430

    change project

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { isFocusable } from "@testing-library/user-event/dist/utils";
-export default function App() {
-  const [isFocus, setFocus] = useState(false);
-  const [img, setImg] = useState("https://picsum.photos/200");
-  const [clientX, setClientX] = useState(null);
-  const [clientY, setClientY] = useState(null);
-  useState(null);
-  function mouseMove(p) {
-    setFocus(!isFocus);
-    setClientX(p.clientX)
-    setClientY(p.clientY)
-    console.log(p.clientX);
-    console.log(p.clientY);
+import React from 'react'
 
-  }
+export default function App() {
   return (
     <div>
-      <img
-        onMouseMove={(p) => {
-          mouseMove(p);
-        }}
-        src={img}
-        alt=""
-      />
-      {isFocus === true && (
-        <div
-          style={{
-            backgroundImage: `url(${img})`,
-            backgroundPosition: `right ${clientX * 2 - 300}px  bottom ${clientY * 2 - 200}px`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-          className="imageZoom"
-        ></div>
-      )}
-      <div></div>
+
+      
     </div>
-  );
+  )
 }
