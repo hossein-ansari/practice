9f924202d1f73c36ff9a32bb37d16a20529c32e8
hossein <hossein838485@gmail.com>
2023-05-10 19:05:39 +0430

    add link router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import {Link , Routes,Route } from 'react-router-dom'
 export default function App() {
   return (
     <div>
-
+        <Link to='/3'>lol3</Link>
+      <Routes>
+        <Route path='/' element={<h1>lol1</h1>}></Route>
+        <Route path='/3' element={<h3>lol3</h3>}></Route>
+        <Route path='/2' element={<h2>lol2</h2>}></Route>
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
