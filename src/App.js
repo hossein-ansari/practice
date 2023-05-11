42cfcd60f4722a50b18af1356347dfca05be92f4
hossein <hossein838485@gmail.com>
2023-05-11 23:57:30 +0430

    change

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ export default function App() {
         <Link to='/3'>lol3</Link>
       <Routes>
         <Route path='/' element={<h1>lol1</h1>}></Route>
-        <Route path='/3' element={<h3>lol3</h3>}></Route>
-        <Route path='/2' element={<h2>lol2</h2>}></Route>
+
       </Routes>
     </div>
   )
