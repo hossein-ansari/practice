2d7507a62140ec09f4672505668412c8b26b6327
hossein <hossein838485@gmail.com>
2023-05-13 15:32:49 +0430

    add data to file for use params

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link , Routes,Route } from 'react-router-dom'
+import Datas from './Datas'
 export default function App() {
+  const [post ,usePost] = useState(Datas)
   return (
     <div>
         <Link to='/3'>lol3</Link>
       <Routes>
-        <Route path='/' element={<h1>lol1</h1>}></Route>
+        {post.map(post =>(
+         <Link to='/post.id'></Link>
+        ))}
 
       </Routes>
     </div>
---
+++ src/Datas.js
@@ -0,0 +1,9 @@
+let posts = [
+
+    {id:1,name:'ali'},
+    {id:2,name:'puria'},
+    {id:3,name:'hossein'},
+    {id:4,name:'mohamad'},
+
+]
+export default posts
