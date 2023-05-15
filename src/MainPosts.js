5577cad668f09df767094a38c60072d746ab5da9
hossein <hossein838485@gmail.com>
2023-05-15 16:41:46 +0430

    add navigate

--- src/MainPosts.js
+++ src/MainPosts.js
@@ -1,14 +1,21 @@
 import React,{useState} from "react";
-import { useParams } from "react-router-dom";
+import { useParams,Navigate } from "react-router-dom";
 import Datas from "./Datas";
 
 export default function MainPosts() {
   const params = useParams();
   const [post, usePost] = useState(Datas);
+  const hasPost = post.some(post => post.id == params.ID)
   console.log('lol');
   return (
     <div>
-    {post.find(post => post.id == params.ID).name}
+      {!hasPost ? (
+      <> <Navigate to='/posts' /> </>
+      ): (
+      <> {post.find(post => post.id == params.ID).name} 
+      </>)
+      }
+    
     </div>
   );
 }
