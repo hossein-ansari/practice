7dd78a27483fc3b0ea13102dc4d88cfcb5026e84
hossein <hossein838485@gmail.com>
2023-05-18 16:55:46 +0430

    practice

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ export default function App() {
         <Route path="/posts" element={<Posts />} />
         <Route path="/post/:ID" element={<Mainposts />} />
         <Route path="*" element={<NotFound />} />
-
       </Routes>
     </>
   );
