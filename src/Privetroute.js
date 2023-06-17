0b23c86b585a41b25e46089e4ba7ab31d12d0314
hossein <hossein838485@gmail.com>
2023-06-17 13:09:18 +0430

    add privet routes

--- src/Privetroute.js
+++ src/Privetroute.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {islogin} from "./utils.js";
 import { Outlet } from "react-router-dom";
-import Panel from "./Panel";
-
 import { Navigate } from "react-router-dom";
 export default function Privetroute() {
   let login = islogin("adf");
