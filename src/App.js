6f3553b4233f2da6c855618b7df947c55754b6a3
hossein <hossein838485@gmail.com>
2023-06-19 16:53:20 +0430

    try profiler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState ,Profiler } from "react";
 import Home from "./Home.js";
 export default function App() {
+  function renderInfo(actualDureation,baseDuration,startTime, commitTimegit) {
+      console.log(actualDureation,baseDuration ,startTime, commitTime);
+  }
   return (
     <>
     <Profiler id={'home-Page'} onRender={renderInfo}>
