6b0566214334685e10ebc3290f8f2c93b12e9a73
hossein <hossein838485@gmail.com>
2023-06-19 16:47:45 +0430

    practice profiler

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Home from "./Home.js";
 export default function App() {
   return (
     <>
+    <Profiler id={'home-Page'} onRender={renderInfo}>
       <Home />
+      </Profiler>
     </>
   );
 }
