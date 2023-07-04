01787f93655621a06387b28b3431363922272566
hossein <hossein838485@gmail.com>
2023-07-04 12:30:55 +0430

    add ts to project and practice static value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import React, { useState } from "react";
-import { useRoutes } from "react-router-dom";
-import Datas from "./Datas";
-import Routes from "./Routes";
-import Home from "./Home.js";
-import ContexProvider from "./Contex";
-import ChildComponent from "./ChildComponent";
-export default function App() {
-  const [post, usePost] = useState(Datas);
-  const router = useRoutes(Routes);
-  return (
-    <ContexProvider>
-      <Home />
-      {router}
-      <ChildComponent />
-    </ContexProvider>
-  );
-}
+var num = 5;
+var datas = {
+    id: 5,
+    name: "ali",
+};
+console.log(datas);
---
+++ src/App.ts
@@ -0,0 +1,7 @@
+const num: number = 5;
+let datas: { id: number; name: string } = {
+  id: 5,
+  name: "ali",
+};
+console.log(datas);
+
