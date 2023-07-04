da89e3b81f4f1a72037bef73e5b3c9c9995a33d9
hossein <hossein838485@gmail.com>
2023-07-04 19:50:11 +0430

    try tuple

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-var num = 5;
-var datas = {
-    id: 5,
-    name: "ali",
-};
-console.log(datas);
+// const num: number = 5;
+// let datas: { id: number; name: string } = {
+//   id: 5,
+//   name: "ali",
+// };
+// let arry : (string|number)[]=['ali','mamad','soghra',12]
+// console.log(arry);
+var a = 2;
+a = 'li';
+console.log(a);
--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,15 @@
+
+// anotation 
 const num: number = 5;
+// inference
+const num = 5; 
+// arry and obj 
 let datas: { id: number; name: string } = {
   id: 5,
   name: "ali",
 };
-console.log(datas);
+let arry : (string|number)[]=['ali','mamad','soghra',12]
+console.log(arry);
+// tuple
+let arry2 :[string,number]=['mike',2]
 
