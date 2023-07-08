79ae863dba37a1a9fb4c5a8cb4017b79d7737ddb
hossein <hossein838485@gmail.com>
2023-07-08 14:07:21 +0430

    add type castinh

--- src/cheatshit.js
+++ src/cheatshit.js
@@ -3,33 +3,38 @@ var num1 = 5;
 // inference
 var num = 5;
 var num3 = 5;
-num3 = 'lol';
 // arry and obj
 var datas = {
     id: 5,
     name: "ali",
 };
 var arry = ["ali", "mamad", "soghra", 12];
-console.log(arry);
-console.log(num3);
-// // tuple
-// let arry2: [string, number] = ["mike", 2];
-// // functions assign value
-// function isLogin(user: String, password: number):boolean {
-//   if (user === 'admin' && password === 1111) {
-//     return true
-//   }
-//   else{
-//     return false
-//   }
-// }
+// console.log(arry);
+// console.log(num3)
+// tuple
+var arry2 = ["mike", 2];
+// functions assign value
+function isLogin(user, password) {
+    if (user === 'admin' && password === 1111) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 // console.log(isLogin('admin',1111))
-// // any type 
-// let testAny:any = 12
-// testAny = 'owu'
+// any type 
+var testAny = 12;
+testAny = 'owu';
 // console.log(testAny)
-// // literal type 
-// let testLiteral: (18|19|20)=18
-// testLiteral = 20
+// literal type 
+var testLiteral = 18;
+testLiteral = 20;
 // console.log(testLiteral)
-// // 'unknown' is like 'any' type but 'any' dont have type checking
+// 'unknown' is like 'any' type but 'any' dont have type checking
+// type casting 
+var typeCasted = 12;
+var typeCasted2 = 12;
+var btn = document.querySelector('button');
+// let btn = document.querySelector('button')!
+console.log(btn.innerHTML);
--- src/cheatshit.ts
+++ src/cheatshit.ts
@@ -3,33 +3,39 @@ const num1: number = 5;
 // inference
 const num = 5;
 let num3 = 5;
-num3 = 'lol'
 // arry and obj
 let datas: { id: number; name: string } = {
   id: 5,
   name: "ali",
 };
 let arry: (string | number)[] = ["ali", "mamad", "soghra", 12];
-console.log(arry);
-console.log(num3)
-// // tuple
-// let arry2: [string, number] = ["mike", 2];
-// // functions assign value
-// function isLogin(user: String, password: number):boolean {
-//   if (user === 'admin' && password === 1111) {
-//     return true
-//   }
-//   else{
-//     return false
-//   }
-// }
+// console.log(arry);
+// console.log(num3)
+// tuple
+let arry2: [string, number] = ["mike", 2];
+// functions assign value
+function isLogin(user: String, password: number):boolean {
+  if (user === 'admin' && password === 1111) {
+    return true
+  }
+  else{
+    return false
+  }
+}
 // console.log(isLogin('admin',1111))
-// // any type 
-// let testAny:any = 12
-// testAny = 'owu'
+// any type 
+let testAny:any = 12
+testAny = 'owu'
 // console.log(testAny)
-// // literal type 
-// let testLiteral: (18|19|20)=18
-// testLiteral = 20
+// literal type 
+let testLiteral: (18|19|20)=18
+testLiteral = 20
 // console.log(testLiteral)
-// // 'unknown' is like 'any' type but 'any' dont have type checking
+// 'unknown' is like 'any' type but 'any' dont have type checking
+// type casting 
+let typeCasted = 12 as const
+let typeCasted2 = <const>12
+let btn = document.querySelector('button') as HTMLButtonElement
+// let btn = document.querySelector('button')!
+
+console.log(btn.innerHTML)
